551ac8ee4566040a645682f12dacb8ffd2761f7c
Feng Yue <fengyue@zju.edu.cn>
2016-01-22 18:48:31 +0800

    T.d is a small test mesh.

---
+++ RBEC--TotalMat/RBEC-TotalMat/T.d
@@ -0,0 +1,26 @@
+#--------------------------------#
+# Example: for domain [-6,6]x[-6,6]#
+#--------------------------------#
+
+#=========
+| POINTS |
+=========#
+4 # number of points #
+
+# Nodes which define the boundary #
+0:  -6  -6   6.0   1
+1:  6   -6   6.0   1
+2:  6   6    6.0   1
+3:  -6  6    6.0   1
+
+#===========
+| SEGMENTS |
+===========#
+4 # Number of segments #
+
+# Boundary segments #
+0:   0   1   1
+1:   1   2   1
+2:   2   3   1
+3:   3   0   1
+
